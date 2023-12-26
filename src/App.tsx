776981d9506f351580b2a5e7e111713fe9149a6c
DeanSui <dean@57blocks.com>
2023-12-26 10:52:32 +0800

    mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,40 @@
-import React, { useState } from 'react';
-import { DatePicker, DatePickerProps, Space, Button, Switch, Slider, Image, InputNumber } from 'antd';
+import React, { useCallback, useState } from 'react';
+import { DatePicker, DatePickerProps, Space, Button, Switch, Slider, Image, InputNumber, Flex, Popconfirm, Select, Divider } from 'antd';
 import { GpsSender } from './utils';
 import './my.css'
-import { useDebounceEffect } from 'ahooks';
+import { MinusOutlined, PlusOutlined, SyncOutlined } from '@ant-design/icons';
+import dayjs from 'dayjs';
 
 const gpsSender = new GpsSender();
 
-// 我需要一个定时器，每隔一段时间，就生成一个数据，然后将数据传给后端
-// 生成数据的时候，需要将经纬度和时间字符串传给后端
+// I need a timer that generates data at regular intervals and then sends the data to the backend.
+// When generating the data, I need to send the latitude, longitude, and time string to the backend.
 
-// currentLat 和 currentLon 我希望只保留小数点后四位
+// I want to keep only four decimal places for currentLat and currentLon.
 
 const App: React.FC = () => {
     const [dateTimeValue, setDateTimeValue] = useState('');
     const [directValue, setDirectValue] = useState(0);
+    const [cornerTime, setCornerTime] = useState(5); // [s]
+    const [targetDirectValue, setTargetDirectValue] = useState(0);
     const [isStart, setIsStart] = useState(false);
+    const [ongoing, setOngoing] = useState(false);
     const [currentDateTime, setCurrentDateTime] = useState(new Date());
+    const [rmDate, setRmDate] = useState<string>(); // [s
     const [firstSendGpsPoint, setFirstSendGpsPoint] = useState(true);
+    const [timeoutList, setTimeoutList] = useState<number[]>([]);
+    const [mode, setMode] = useState<number>(1);
 
     const [currentLat, setCurrentLat] = useState(0);
     const [currentLon, setCurrentLon] = useState(0);
 
     const marks = {
-        0: '北',
-        90: '东',
-        180: '南',
-        270: '西',
-        359: '北'
+        0: 'North',
+        90: 'East',
+        180: 'South',
+        270: 'West',
+        359: 'North'
       };
-
-    // dateTimeValue 需要在定时器开始后，根据时间间隔，来更新时间字符串
-    useDebounceEffect(
-    () => {
-        if (dateTimeValue && isStart) {
-            console.log('currentDateTime: ', currentDateTime);
-
-            // 需要格式化为 2021-08-31T16:00:00+08:00
-            // const currentDatetimeStr = currentDateTime.toISOString().replace(/\.\d{3}Z$/, 'Z')
-            const currentDatetimeStr = currentDateTime.toISOString();
-            const baseParams = {
-                lat: currentLat,
-                lon: currentLon,
-                datetime: currentDatetimeStr,
-            }
-            // gpsSender.sendGpsPoint(newLat, newLon, timeString);
-            const params = {
-                isStart: isStart,
-                direct: 0,
-                direction: directValue
-            }
-            gpsSender.sendGpsPoint(firstSendGpsPoint ? {...params, ...baseParams} : params).then((res) => {
-                console.log('res: ', res);
-                setFirstSendGpsPoint(false);
-            }).catch((e) => {
-                console.log('error', e);
-            })
-
-        }
-    },
-    [isStart, dateTimeValue, currentLat, currentLon, directValue, currentDateTime],
-    {
-        wait: 1000,
-    },
-    );
 
     const onDateTimeOk = (value: DatePickerProps['value']) => {
         const v = value!.format('YYYY-MM-DDTHH:mm:ssZ');
@@ -71,6 +43,21 @@ const App: React.FC = () => {
         setDateTimeValue(v)
     };
 
+    const deleteTimeDataChange = (value: DatePickerProps['value']) => {
+        const date = value!.format('YYYY-MM-DD');
+        setRmDate(date);
+    }
+
+    const confirm = () => {
+        console.log('rmDate: ', rmDate);
+        gpsSender.rmdata(rmDate!).then((res) => {
+            console.log('res: ', res);
+            setRmDate(undefined);
+        }).catch((e) => {
+            console.log('error', e);
+        })
+    }
+
     const onDirectChange = (e: number | null) => {
         if (e === null) {
             return;
@@ -78,8 +65,60 @@ const App: React.FC = () => {
         setDirectValue(e);
     };
 
-    const onSwitchChange = (checked: boolean) => {
+    const submitData = useCallback(async (checked: boolean, defaultDirectValue?: number) => {
+        if (!checked) {
+            gpsSender.sendGpsPoint({
+                isStart: false,
+                direct: 0,
+                direction: directValue,
+            }).then((res) => {
+                console.log('res: ', res);
+            }).catch((e) => {
+                console.log('error', e);
+            })
+        }
+        if (checked) {
+            const currentDatetimeStr = currentDateTime.toISOString();
+            const baseParams = {
+                lat: currentLat,
+                lon: currentLon,
+                datetime: currentDatetimeStr,
+            }
+            // gpsSender.sendGpsPoint(newLat, newLon, timeString);
+            const params = {
+                isStart: checked,
+                direct: 0,
+                direction: typeof defaultDirectValue === 'number'  ? defaultDirectValue : directValue,
+                mode: mode,
+            }
+            if (firstSendGpsPoint) {
+                setFirstSendGpsPoint(false);
+                await gpsSender.sendGpsPoint({...params, ...baseParams}).then((res) => {
+                    console.log('res: ', res);
+                }).catch((e) => {
+                    console.log('error', e);
+                })
+            } else {
+                await gpsSender.sendDirection(params).then((res) => {
+                    console.log('res: ', res);
+                    // setFirstSendGpsPoint(false);
+                }).catch((e) => {
+                    console.log('error', e);
+                })
+            }
+        }
+    }, [currentDateTime, currentLat, currentLon, directValue, firstSendGpsPoint, mode])
+
+    const onSwitchChange = async (checked: boolean) => {
         setIsStart(checked);
+        if (!checked) {
+            setOngoing(false);
+            setFirstSendGpsPoint(true);
+            timeoutList.forEach((timeout) => {
+                clearTimeout(timeout);
+            })
+        }
+        await submitData(checked);
     };
 
     const onResetData = () => {
@@ -89,56 +128,170 @@ const App: React.FC = () => {
         setCurrentLon(0);
     };
 
+    const onPlusClick = () => {
+        setDirectValue(directValue + 1);
+        submitData(isStart, directValue + 1);
+    }
+
+    const onMinusClick = () => {
+        setDirectValue(directValue - 1);
+        submitData(isStart, directValue - 1);
+    }
+
+    const onStart = () => {
+        const unitDirect = (targetDirectValue - directValue) / cornerTime;
+        if (unitDirect === 0) {
+            return;
+        }
+        setOngoing(true);
+        const timeoutList = [];
+        for (let i = 1; i <= cornerTime; i++) {
+            const timeout = setTimeout(() => {
+                setDirectValue(directValue + unitDirect*i);
+                submitData(isStart, directValue + unitDirect*i);
+                if (i === cornerTime) {
+                    setOngoing(false);
+                }
+            }, i * cornerTime * 500);
+            timeoutList.push(timeout);
+        }
+        setTimeoutList(timeoutList);
+    };
+
     return (
         <div className='container'>
             <nav className='navbar'>
                 <Image width={40} height={40} src="/svv.png" />
                 <a className='a' href='/'>Spray Data Simulator</a>
                 <Space direction="horizontal" size={20} align="center">
-                    <Button onClick={onResetData}>数据重置</Button>
-                    <Switch onChange={onSwitchChange} checkedChildren="开启" unCheckedChildren="暂停" value={isStart} />
+                    <Button onClick={onResetData}>Data reset</Button>
+                    <Switch onChange={onSwitchChange} checkedChildren="Enable" disabled={!dateTimeValue} unCheckedChildren="Pause" value={isStart} />
                 </Space>
             </nav>
 
             <div className='content'>
                 <div className='left-content'>
                     <div className='first-bar'>
-                        <h3>初始位置设定</h3>
+                        <h3>Set initial position</h3>
                         <Space direction="vertical" size={20} align="start">
                             <div className='one'>
                                 <label htmlFor="lat" style={{ marginRight: '10px' }}>Lat: </label>
                                 <InputNumber min={-90} max={90} id="lat" name="lat" value={currentLat} disabled={isStart} onChange={(e) => {
                                     setCurrentLat(Number(e))
-                                }} />
+                                }}
+                                addonAfter={<SyncOutlined onClick={() => {
+                                    setCurrentLat(Number((Math.random() * 180 - 90).toFixed(4)))
+                                }} />} />
                             </div>
                             <div className='one'>
                                 <label htmlFor="lon" style={{ marginRight: '10px' }}>lon: </label>
                                 <InputNumber min={-180} max={180} type="number" id="lon" name="lon" disabled={isStart} value={currentLon} onChange={(e) => {
                                     setCurrentLon(Number(e))
-                                }} />
+                                }}
+                                addonAfter={<SyncOutlined onClick={() => {
+                                    setCurrentLon(Number((Math.random() * 360 - 180).toFixed(4)))
+                                }} />}/>
                             </div>
                         </Space>
                     </div>
 
                     <div className='first-bar'>
-                        <h3>初始时间设定</h3>
+                        <h3>Set initial time</h3>
                         <Space direction="vertical" size={10} align="start">
-                            <DatePicker showTime onOk={onDateTimeOk} disabled={isStart} />
+                            <DatePicker showTime onChange={onDateTimeOk} disabled={isStart} />
                         </Space>
                     </div>
 
                     <div className='first-bar'>
-                        <h3>方向</h3>
-                        <Slider min={0} max={359} marks={marks} defaultValue={directValue} onChange={onDirectChange} value={directValue}/>
-                        <InputNumber
-                            min={0}
-                            max={359}
-                            style={{ margin: '16px 0' }}
-                            value={directValue}
-                            onChange={onDirectChange}
-                            addonAfter="°"
-                        />
+                        <h3>Path pattern</h3>
+                        <Space direction="vertical" size={10} align="start">
+                            <Select
+                                disabled={isStart}
+                                value={mode}
+                                onChange={(e) => {
+                                    setMode(e);
+                                }}
+                                options={[
+                                    { value: 1, label: 'Lateral zigzag route' },
+                                    { value: 2, label: 'Vertical zigzag route' },
+                                ]}
+                            />
+                        </Space>
                     </div>
+
+                    {
+                        false &&
+                        <div className='first-bar'>
+                            <h3>Set initial direction</h3>
+                            <Slider min={0} max={359} marks={marks} disabled={isStart} defaultValue={directValue} onChange={onDirectChange} value={directValue}/>
+                            <InputNumber
+                                min={0}
+                                max={359}
+                                style={{ margin: '16px 0' }}
+                                value={directValue}
+                                onChange={onDirectChange}
+                                addonAfter="°"
+                                disabled={isStart}
+                            />
+                            {
+                                false && <Flex gap="middle" vertical>
+                                <Button onClick={() => onPlusClick()} disabled={!isStart || directValue>=359 || ongoing}><PlusOutlined /></Button>
+                                <Button onClick={() => onMinusClick()} disabled={!isStart || directValue<=0 || ongoing}><MinusOutlined /></Button>
+                            </Flex>
+                            }
+                        </div>
+                    }
+                    <Divider style={{marginBottom: 0}} />
+                    <div className='first-bar'>
+                        <h3>Delete selected date data</h3>
+                        <Space direction='horizontal' size={10} align="start">
+                            <DatePicker onChange={deleteTimeDataChange} disabled={isStart} value={rmDate ? dayjs(rmDate) : undefined} />
+                            <Popconfirm
+                                title="Delete data?"
+                                description="Are you sure you want to delete the data for this day?"
+                                onConfirm={confirm}
+                                // onCancel={cancel}
+                                okText="Yes"
+                                cancelText="No"
+                            >
+                                <Button danger disabled={!rmDate}>Delete</Button>
+                            </Popconfirm>
+                        </Space>
+                    </div>
+                    
+                    {
+                        isStart && false && <>
+                            <div className='first-bar'>
+                                <h3>Corner time (s)</h3>
+                                <InputNumber
+                                    min={1}
+                                    max={10}
+                                    style={{ margin: '16px 0' }}
+                                    value={cornerTime}
+                                    onChange={(e) => setCornerTime(Number(e) || 1)}
+                                    addonAfter="s"
+                                    disabled={ongoing}
+                                />
+                            </div>
+
+                            <div className='first-bar'>
+                                <h3>Set target direction</h3>
+                                <Slider min={0} max={359} marks={marks} disabled={ongoing} defaultValue={targetDirectValue} onChange={(e) => setTargetDirectValue(Number(e))} value={targetDirectValue}/>
+                                <InputNumber
+                                    min={0}
+                                    max={359}
+                                    style={{ margin: '16px 0' }}
+                                    value={targetDirectValue}
+                                    onChange={e => setTargetDirectValue(Number(e))}
+                                    addonAfter="°"
+                                    disabled={ongoing}
+                                />
+                                <Flex gap="middle" vertical>
+                                    <Button onClick={() => onStart()} loading={ongoing}>Start</Button>
+                                </Flex>
+                            </div>   
+                        </>
+                    }
                 </div>
 
                 <div className='right-content'>
--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,7 @@ interface Point {
     lon?: number;
     direct: number;
     datetime?: string;
+    mode?: number;
     isStart: boolean;
     direction: number;
 }
@@ -18,6 +19,42 @@ export class GpsSender {
                     'Content-Type': 'application/json',
                 },
                 body: JSON.stringify(point),
+            });
+
+            if (!response.ok) {
+                throw new Error(`Server responded with ${response.status}`);
+            }
+        } catch (error) {
+            console.error('Failed to send GPS point:', error);
+        }
+    }
+
+    async sendDirection(point: Point): Promise<void> {
+        try {
+            const response = await fetch('/direction', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(point),
+            });
+
+            if (!response.ok) {
+                throw new Error(`Server responded with ${response.status}`);
+            }
+        } catch (error) {
+            console.error('Failed to send GPS point:', error);
+        }
+    }
+
+    async rmdata(date: string): Promise<void> {
+        try {
+            const response = await fetch('/rmdata', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({date}),
             });
 
             if (!response.ok) {
